c03fb9f30558ea2b491438c2aef3b98ff663b927
Shawn Milochik <shawn@milochik.com>
2021-03-26 09:38:34 -0400

    db cleanup

--- db.go
+++ db.go
@@ -44,28 +44,14 @@ func initializeDB() {
 }
 
 func initializeFib() {
-	row := db.QueryRow("SELECT id FROM fibonacci LIMIT 1")
-	var i int
-	err := row.Scan(&i)
-	if err == nil {
-		return
-	}
-	_, err = db.Exec("CREATE TABLE fibonacci (id INTEGER PRIMARY KEY, val INTEGER)")
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS fibonacci (id INTEGER PRIMARY KEY, val INTEGER)")
 	if err != nil {
 		log.Printf("table creation failed: %s", err)
-	} else {
-		log.Println("created table OK")
 	}
 }
 
 func initializeBelow() {
-	row := db.QueryRow("SELECT id FROM below LIMIT 1")
-	var i int
-	err := row.Scan(&i)
-	if err == nil {
-		return
-	}
-	_, err = db.Exec("CREATE TABLE below (id INTEGER PRIMARY KEY, count INTEGER)")
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS below (id INTEGER PRIMARY KEY, total INTEGER)")
 	if err != nil {
 		log.Printf("table creation failed: %s", err)
 	}
@@ -84,8 +70,8 @@ func memoizeFib(v Value) {
 }
 
 // Store "below" data in database.
-func memoizeBelow(num int, count int) {
-	_, err := db.Exec("INSERT INTO below (id, count) VALUES ($1, $2)", num, count)
+func memoizeBelow(num int, total int) {
+	_, err := db.Exec("INSERT INTO below (id, total) VALUES ($1, $2)", num, total)
 	if err != nil && !isDuplicate(err) {
 		log.Printf("failed to write to DB: %s", err)
 		return
@@ -111,7 +97,7 @@ func ordinalFromDB(id int) (Value, error) {
 }
 
 func belowFromCache(id int) (int, bool) {
-	row := db.QueryRow("SELECT count FROM below WHERE id = $1", id)
+	row := db.QueryRow("SELECT total FROM below WHERE id = $1", id)
 	var i int
 	var ok bool
 
